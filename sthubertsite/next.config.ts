138349c81c579fc4f322507ad2cbc6b636bea0bd
StHubertSite <ham4josh+sthubert@proton.me>
2025-02-01 01:03:05 -0500

    Initial Page

---
+++ sthubertsite/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+  // basePath: "/2048-in-react",
+};
+
+export default nextConfig;
