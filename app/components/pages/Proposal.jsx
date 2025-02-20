0ad62c225232dd2e47ccd50e55c212692ce015b5
StHubertSite <ham4josh+sthubert@proton.me>
2025-02-20 13:33:12 -0500

    Added proposal page.

---
+++ app/components/pages/Proposal.jsx
@@ -0,0 +1,205 @@
+import React from "react";
+import "./pages.css";
+import TransparentBackButton from "../TransparentBackButton";
+
+const Proposal = ({ handleBack }) => {
+  document.getElementById("PageTitle").innerText = "Olivia, will you marry me?";
+
+  return (
+    <div className="visit-container">
+      <div className="cards-grid">
+        <div className="card-column">
+          <div className="card">
+            <h2>Yes!</h2>
+            <img src="JoshAndOlivia/Proposal/2024-08-10-0016.JPG" />
+          </div>
+          <div className="card">
+            <h2>Proposal Story</h2>
+            <p>Picnic and story here!</p>
+            <p>
+              Laura went to trader joes to grab food for the party that night.
+              She also picked up food for our picnic and picked the ring up for
+              me.
+              <br />
+              She gave me the ring and then gave us the paper bag with food.
+              <br />
+              <br />I drove Olivia to the farm for the picnic and we drove down
+              to the loop. We got out and wanted to sit by the old concrete
+              building next to the barn. There were two small cows hanging out
+              there so we looked for another spot.
+              <br />
+              We went to the other side of the barn and concrete house, and it
+              was clear. We made our way around the manure and found a nice spot
+              in the grass. We put down the blanket, it was a child design
+              featuring the continents and animals. There was a giraffe and
+              south America.
+              <br />
+              We opened our bottles of kombucha, and Olivia’s sprayed on her. I
+              asked if she needed napkins from my car, but she told me she was
+              fine.
+              <br />
+              We both ate a chicken wrap and had chips, banana chips, and I had
+              some chocolate mints. It was all good and we talked a bit. It was
+              a nice picnic, but I was not super hungry and wanted food to
+              digest. I asked Olivia to take a walk with me.
+              <br />
+              We were already at the barn so the walk down the road would not
+              have been very long. When we got to the garden, I asked Olivia if
+              we could go into a field. The field was full of morning glories
+              and blue chicories. The grass was tall, and the weather was
+              perfect! There were some clouds, and the sun shone on us, Olivia’s
+              face glowed.
+              <br />
+              We crawled underneath the electric wire careful not to touch it. I
+              took her hand and walked to the center of the field. I pulled out
+              my Litany of Trust and asked her to pray it with me. I led and she
+              followed. Afterwards I asked to pray so we closed our eyes, and I
+              put my hands on hers in a prayerful way. I thanked God for today
+              and the weather, our beautiful relationship, stressing how God is
+              at the center of it all. My focus was to make God a part of the
+              proposal because He is the center of our relationship. The Holy
+              Spirit is the love that brings Olivia and me together. As I finish
+              the prayer, I move my left hand to my back left pocket glancing to
+              see that Olivia’s eyes are still closed. I feel the small, skinny
+              box in my hand and pull it out, keeping it behind my back. I
+              fiddle with it while still praying, trying to get the box opened.
+              As we finish the prayer, I get down on my right knee and pull the
+              box out.
+              <br />
+              “Olivia, Will you marry me?”
+              <br />
+              “I will!”
+              <br />
+              Olivia takes the ring and slides it onto her ring finger. She
+              examines the ring’s beauty and is overfilled with joy. She falls
+              into my arms and begins to cry. I held her, comforting and
+              expressing my love for her. We kiss and are both overjoyed.
+              <br />
+              <br />
+              <br />
+              We walk in the field; I lift Olivia skyward and want to spin her
+              around! She is all mine, and I am all hers! I hug her tight and
+              she wraps her arms around my head. We kiss and I bring her down. I
+              ask her if it feels like a dream.
+              <br />
+              “Yes!”
+              <br />I ask Olivia if it feels real.
+              <br />
+              “No”
+              <br />
+              It is the most amazing feeling in the whole world. Nothing else
+              mattered but our love.
+              <br />I ask if she will run with me. We hold hands and spring
+              through the flowers. I ran ahead a couple yards and caught her.
+              She runs into my arms, and I lift her high! Olivia is the love of
+              my life and so, so good!
+              <br />
+              We continue to walk and talk and share our feelings.
+              <br />
+              <br />
+              <br />
+              Leaving the field, we crawl under the electric fence. We are
+              careful to avoid getting shocked and I comment/think about if she
+              got engaged then shocked! Luckily, we did not and walked back to
+              the barn to finish our picnic. Olivia climbs over the fence first
+              and “OWWWW!”
+              <br />
+              Her bare shoulder brushed against the electric wire that goes
+              straight up to the roof. It was in the shade and was basically
+              invisible. I felt so sorry for her and rubbed the irritating mark.
+              I climbed over the fence and kissed her burn. We laughed about it
+              and continued to the picnic spot.
+              <br />
+              We returned to the spot where we had moved our bag. I had draped
+              the blanket over the bag to protect the contents from the sun and
+              animals. There was a curious little cow roaming around the bag. We
+              take the blanket and bag, and the calf follows up. We put down the
+              blanket and the cow stayed close to the bag as if it is expecting
+              to be fed! The calf is nice company other than the flies.
+              <br />
+              <br />
+              Olivia pulls out one of the salads for us to share. She opened the
+              dressing and spilled it on herself. I thought “I should have
+              grabbed the napkins when we were coming back!” I got over the
+              fence and to the car to grab napkins. When I climbed back over the
+              fence, I saw that the cow had indeed followed me! I rushed back to
+              Olivia and the cow looked for me in the barn. I clean up the
+              dressing for Olivia and pour it on the salad.
+              <br />
+              During this time, I thought about how she was going to show her
+              family, especially her siblings.
+              <br />
+              <br />
+              Back at my car I get my camera out since all my belongings were
+              packed in the car. I take some shots of Olivia and closeups of the
+              ring. She looks incredible! She is just of pretty and her whole
+              face was shining!
+              <br />
+              <br />
+              <br />
+              When we got back to the house, I expected a lot of commotion, but
+              it was quiet. Her parents are in the office space, and she
+              immediately showed them. Laura gave Olivia a hug and both were so
+              excited! Everyone else is in the backyard so she rushed out to
+              show them. They ran up to her and Anna was very excited. Faith
+              made some comment and Cora asked something? Iris was at work and
+              Joel was with friends. Olivia immediately sent a picture to Joel
+              that she quickly took with her flip phone. He calls Olivia and
+              asks what we are doing and why!
+              <br />
+              Laura gets her camera and takes us to the backyard. We stand with
+              each other in front of the trees by the patio and she takes great
+              pictures! I go the computer and copy them onto mine and Olivia’s
+              phones.
+              <br />
+              <br />
+              <br />
+              After a while I finally must leave. Faith and Olivia help me bring
+              my things to my car and I say goodbye. Cora and Fatih would not
+              leave me alone, so I blew Olivia a kiss and drove off.
+            </p>
+          </div>
+        </div>
+        <div className="card-column">
+          <TransparentBackButton onClick={handleBack} text={"Back"} />
+          <div className="card">
+            <img src="JoshAndOlivia/Proposal/2024-07-04-0016.JPG" />
+            <h2>Would you take my hand?</h2>
+            <p>And walk with me forever more?</p>
+          </div>
+          <div className="card">
+            <p>
+              I love you so much,
+              <br />
+              my Olivia Noel
+              <br />
+              <br />
+              I love you so much,
+              <br />
+              my Olivia Noel
+              <br />
+              <br />I love you, I love you,
+              <br />
+              my sweet Olivia Noel
+            </p>
+          </div>
+        </div>
+        <div className="card-column">
+          <div className="card">
+            <img src="JoshAndOlivia/Proposal/2024-08-10-0015.JPG" />
+          </div>
+          <div className="card">
+            <h2>Jesus, I trust in you</h2>
+            <img src="JoshAndOlivia/Proposal/Litany.JPG" />
+            <p>
+              Jesus, I love you
+              <br /> I love you, Jesus
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Proposal;
